177b6a72b63fd08585f21784dcc95831444d478d
ilmari99 <i.vahteristo@gmail.com>
2022-09-03 18:04:30 +0300

    added support for the Threaded moska

--- moska.py
+++ moska.py
@@ -1,9 +1,9 @@
 import Deck
-from Game import MoskaGame
-from Player import MoskaPlayerBase
+from Game import MoskaGame, MoskaGameThreaded
+from Player import MoskaPlayerBase, MoskaPlayerThreadedBase
 import sys
 
-if __name__ == "__main__":
+def start_moska():
     moskafile = open("./moskafile.txt","w",encoding="utf-8")
     sys.stdout = moskafile
     deck = Deck.StandardDeck()
@@ -15,3 +15,19 @@ if __name__ == "__main__":
         moskaGame.add_player(player)
     moskaGame.start()
     moskafile.close()
+    
+def start_threaded_moska():
+    moskafile = open("./moskafile_threaded.txt","w",encoding="utf-8")
+    sys.stdout = moskafile
+    deck = Deck.StandardDeck()
+    moskaGame = MoskaGameThreaded(deck)
+    players = []
+    for i in range(5):
+        players.append(MoskaPlayerThreadedBase(moskaGame,pid=i))
+    for player in players:
+        moskaGame.add_player(player)
+    moskaGame.start()
+    
+
+if __name__ == "__main__":
+    start_threaded_moska()
