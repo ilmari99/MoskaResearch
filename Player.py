dd00f0844596f3556f9d711814f58ad44ef7a449
ilmari99 <i.vahteristo@gmail.com>
2022-09-03 18:04:52 +0300

    Added a base player for THreadedMoska

--- Player.py
+++ Player.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
+import multiprocessing
 from typing import TYPE_CHECKING
 from copy import copy, deepcopy
 if TYPE_CHECKING:   # False at runtime, since we only need MoskaGame fro typechekcing
-    from Game import MoskaGame
+    from Game import MoskaGame, MoskaGameThreaded
 from Hand import MoskaHand
 import utils
 from Turns import PlayFallCardFromHand, PlayFallFromDeck, PlayToOther, InitialPlay, EndTurn
 import threading
+import time
 
 
 class MoskaPlayerBase:
@@ -24,6 +26,7 @@ class MoskaPlayerBase:
     rank = None
     thread = None
     name = ""
+    ready = False
     def __init__(self,moskaGame : MoskaGame, pid : int = 0, name : str = ""):
         self.moskaGame = moskaGame
         self.hand = MoskaHand(moskaGame)
@@ -35,6 +38,7 @@ class MoskaPlayerBase:
         self._initialPlay = InitialPlay(self.moskaGame,self)
         self._endTurn = EndTurn(self.moskaGame,self)
         
+        
     def _set_pid(self,pid):
         self.pid = pid
     
@@ -45,15 +49,13 @@ class MoskaPlayerBase:
         """ Return a set of values, that can be played to target"""
         return self._playable_values_to_table().intersection([c.value for c in self.hand])
     
-    def _cards_to_play_to_non_initial_target(self):
-        """ Return the cards that will be played to the target """
-        return 
-    
     def _fits_to_table(self):
         """ Return the number of cards playable to the active/target player"""
         target = self.moskaGame.get_active_player()
         return len(target.hand) - len(self.moskaGame.cards_to_fall)
-        
+    
+    
+    @utils.check_new_card
     def _play_to_target(self):
         """ This method is invoked to play the cards, chosen in 'play_to_target'"""
         play_cards = self.play_to_target()
@@ -65,10 +67,11 @@ class MoskaPlayerBase:
         playable_values = self._playable_values_from_hand()
         play_cards = []
         if playable_values:
-            hand = deepcopy(self.hand)
+            hand = self.hand.copy()
             play_cards = hand.pop_cards(cond=lambda x : x.value in playable_values,max_cards = self._fits_to_table())
         return play_cards
     
+    @utils.check_new_card
     def _play_initial(self):
         target = self.moskaGame.get_active_player()
         play_cards = self.play_initial()
@@ -78,7 +81,7 @@ class MoskaPlayerBase:
         """ Return a list of cards that will be played to target on an initiating turn"""
         target = self.moskaGame.get_active_player()
         min_card = min([c.value for c in self.hand])
-        hand = deepcopy(self.hand)
+        hand = self.hand.copy()
         play_cards = hand.pop_cards(cond=lambda x : x.value == min_card,max_cards = self._fits_to_table())
         return play_cards
     
@@ -89,14 +92,16 @@ class MoskaPlayerBase:
         for card in self.moskaGame.cards_to_fall:
             if utils.check_can_fall_card(deck_card,card):
                 return (deck_card,card)
-
+            
+    @utils.check_new_card
     def _play_fall_from_deck(self):
         self._playFallFromDeck(fall_method=self.deck_lift_fall_method)
 
+    @utils.check_new_card
     def _play_fall_card_from_hand(self):
         play_cards = self.play_fall_card_from_hand()
         self._playFallCardFromHand(play_cards)
-
+    
     def play_fall_card_from_hand(self):
         """ Return a dictionary of card_in_hand : card_in_table -pairs, denoting which card is used to fall which card on the table."""
         play_cards = {}
@@ -105,11 +110,15 @@ class MoskaPlayerBase:
             for play_card in self.hand:
                 success = utils.check_can_fall_card(play_card,fall_card,self.moskaGame.triumph)
                 if success:
-                    cards_that_fall.append(play_card)
-            if cards_that_fall:
-                play_cards[min(cards_that_fall)] = fall_card
+                    if play_card not in play_cards:
+                        play_cards[play_card] = []
+                    play_cards[play_card].append(fall_card)
+        play_cards = {pc : min(fc) for pc,fc in play_cards.items()}
+        pc_inv = {fc : pc for pc,fc in play_cards.items()}
+        play_cards = {pc : fc for fc,pc in pc_inv.items()}
         return play_cards
     
+    @utils.check_new_card
     def _end_turn(self):
         pick_cards = self.end_turn()
         self._endTurn(pick_cards)
@@ -128,19 +137,58 @@ class MoskaPlayerBase:
         
     def _fold(self):
         self.moskaGame.deck.add(self.hand.pop_cards())
-
+        
 
 
 class MoskaPlayerThreadedBase(MoskaPlayerBase):
+    thread : threading.Thread = None
+    moskaGame : MoskaGameThreaded = None
     
-    def _start(self):
-        thread = threading.Thread()
-        
-    def _continuous_play_to_other(self):
-        target = self.moskaGame.get_active_player()
-        while self.moskaGame.get_active_player() == target and self._fits_to_table() > 0:
-            play_cards = self.play_to_target()
-            if play_cards:
-                self._play_to_target()
+    def start(self):
+        #self.thread = threading.Thread(target=self._continuous_play,name=self.name)
+        self.thread = threading.Thread(target=self._continuous_play,name=self.name)
+    
+    def _continuous_play(self):
+        print(f"{self.name} started playing...",flush=True)
+        time.sleep(0.01)
+        initiating_player = None
+        while self.rank is None:
+            with self.moskaGame.main_lock as ml:
+                #current_initiating_player = self.moskaGame.get_initiating_player().pid
+                #if current_initiating_player is not initiating_player:
+                #    initiating_player = current_initiating_player
+                #    initiated = False
+                initiated = False
+                if self.ready and not self.moskaGame.get_active_player() is self:
+                    continue
+                try:
+                    self.ready = True
+                    print(f"{self.name} playing...",flush=True)
+                    print([pl.ready for pl in self.moskaGame.players],flush=True)
+                    if len(self.moskaGame.cards_to_fall + self.moskaGame.fell_cards) > 0:
+                        initiated = True
+                    if len(self.moskaGame.get_players_condition(lambda x : x.rank is None)) <= 1:
+                        break
+                    if self.moskaGame.get_active_player() is self:
+                        cards_on_table = len(self.moskaGame.cards_to_fall)
+                        self._play_fall_card_from_hand()
+                        now_cards_on_table = len(self.moskaGame.cards_to_fall)
+                        if cards_on_table == now_cards_on_table and all([pl.ready for pl in self.moskaGame.get_players_condition(lambda x : x.rank is None)]):
+                            print(f"All players have played the desired cards. Ending {self.name} turn.", flush=True)
+                            self._end_turn()
+                    elif self.moskaGame.get_initiating_player() is self and not initiated:
+                        self._play_initial()
+                        initiated = True
+                    else:
+                        self._play_to_target()
+                except AssertionError as msg:
+                    print(msg, flush=True)
+                print(self.moskaGame,flush=True)
+                if self.rank is not None:
+                    self._end_turn()
+            time.sleep(0.0000000000001)
+        print(f"{self.name} finished as {self.rank}",flush=True)
+        return
+            
         
         
