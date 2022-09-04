49e2432af0bd7221d193561042e2a2030486ae97
ilmari99 <i.vahteristo@gmail.com>
2022-09-04 16:56:51 +0300

    Added support for starting multiple processes to play multiple moska games simultaneously, to possibly gather data in the future.

--- moska.py
+++ moska.py
@@ -1,7 +1,9 @@
+from concurrent.futures import process
 import Deck
 from Game import MoskaGame, MoskaGameThreaded
 from Player import MoskaPlayerBase, MoskaPlayerThreadedBase
 import sys
+import multiprocessing
 
 def start_moska():
     moskafile = open("./moskafile.txt","w",encoding="utf-8")
@@ -16,9 +18,16 @@ def start_moska():
     moskaGame.start()
     moskafile.close()
     
-def start_threaded_moska():
-    moskafile = open("./moskafile_threaded.txt","w",encoding="utf-8")
-    sys.stdout = moskafile
+def start_threaded_moska(file = "moskafile_threaded.txt"):
+    fsplit = file.split(".")
+    file = fsplit[0]
+    ftype = fsplit[-1]
+    #print(file)
+    #print(ftype)
+    stdout = open(file + "."+ftype,"w",encoding="utf-8")
+    stderr = open(file + "_stderr"+"."+ftype, "w", encoding="utf-8")
+    sys.stdout = stdout
+    sys.stderr = stderr
     deck = Deck.StandardDeck()
     moskaGame = MoskaGameThreaded(deck)
     players = []
@@ -27,7 +36,24 @@ def start_threaded_moska():
     for player in players:
         moskaGame.add_player(player)
     moskaGame.start()
+    return True
+    
+def play_games(n=1):
+    processes = []
+    bfile = "moskafile_threaded"
+    for i in range(n):
+        file = bfile + f"({i}).txt"
+        processes.append(multiprocessing.Process(target=start_threaded_moska,args=(file,)))
+    for prc in processes:
+        prc.start()
+    print("Games running...")
+    for prc in processes:
+        prc.join()
+    print("Games finished")
     
 
 if __name__ == "__main__":
-    start_threaded_moska()
+    n = 5
+    play_games(8)
+    
+    
