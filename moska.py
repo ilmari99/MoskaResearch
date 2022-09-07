58a48886e68f94b8b9e9ed05116f1e2c2ea95565
ilmari99 <i.vahteristo@gmail.com>
2022-09-07 10:53:29 +0300

    Removed support for sequential MoskaGames. Updated typing.

--- moska.py
+++ moska.py
@@ -1,37 +1,23 @@
 from Moska import Deck
-from Moska.Game import MoskaGame, MoskaGameThreaded
-from Moska.Player import MoskaPlayerBase, MoskaPlayerThreadedBase
+from Moska.Game import MoskaGame
+from Moska.Player import MoskaPlayer
 import sys
 import multiprocessing
 
-def start_moska():
-    moskafile = open("./moskafile.txt","w",encoding="utf-8")
-    sys.stdout = moskafile
-    deck = Deck.StandardDeck()
-    moskaGame = MoskaGame(deck)
-    players = []
-    for i in range(5):
-        players.append(MoskaPlayerBase(moskaGame,pid=i))
-    for player in players:
-        moskaGame.add_player(player)
-    moskaGame.start()
-    moskafile.close()
     
 def start_threaded_moska(file = "moskafile_threaded.txt"):
     fsplit = file.split(".")
     file = fsplit[0]
     ftype = fsplit[-1]
-    #print(file)
-    #print(ftype)
     stdout = open(file + "."+ftype,"w",encoding="utf-8")
     stderr = open(file + "_stderr"+"."+ftype, "w", encoding="utf-8")
     sys.stdout = stdout
     sys.stderr = stderr
     deck = Deck.StandardDeck()
-    moskaGame = MoskaGameThreaded(deck)
+    moskaGame = MoskaGame(deck)
     players = []
     for i in range(5):
-        players.append(MoskaPlayerThreadedBase(moskaGame,pid=i))
+        players.append(MoskaPlayer(moskaGame,pid=i))
     for player in players:
         moskaGame.add_player(player)
     moskaGame.start()
