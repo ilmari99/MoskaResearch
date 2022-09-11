9921f16ec8bf87adad53df0109f2d9f440c94fa2
ilmari99 <i.vahteristo@gmail.com>
2022-09-11 17:17:29 +0300

    Now prints the winner of a game. Now when making a game, makes randomly about 50/50 MoskaPlayers and MoskaBot1's

--- moska.py
+++ moska.py
@@ -1,10 +1,12 @@
 import logging
+import os
+from time import time
 from Moska import Deck
 from Moska.Game import MoskaGame
-from Moska.Player import HumanPlayer, MoskaPlayer
+from Moska.Player import HumanPlayer, MoskaBot1, MoskaPlayer
 import sys
 import multiprocessing
-
+import random
     
 def start_threaded_moska(file = "moskafile_threaded.txt"):
     fsplit = file.split(".")
@@ -18,10 +20,15 @@ def start_threaded_moska(file = "moskafile_threaded.txt"):
     moskaGame = MoskaGame(deck)
     players = []
     for i in range(5):
-        players.append(MoskaPlayer(moskaGame,pid=i,debug=True,log_file=f"P{i}_"+file+".log"))
+        if random.random() > 0.5:
+            players.append(MoskaPlayer(moskaGame,pid=i,debug=True,log_file=f"P{i}_"+file+".log"))
+        else:
+            players.append(MoskaBot1(moskaGame,pid=i,debug=True,log_file=f"P{i}_"+file+".log"))
     for player in players:
         moskaGame.add_player(player)
-    moskaGame.start()
+    ranks = moskaGame.start()
+    sys.stdout = sys.__stdout__
+    print(ranks[0])
     return True
 
 def play_as_human(nopponents):
@@ -42,14 +49,22 @@ def play_games(n=1):
     for prc in processes:
         prc.start()
     print("Games running...")
-    for prc in processes:
-        prc.join()
+    timeout = 3
+    st = time()
+    while time() - st <timeout:
+        if not any((p.is_alive() for p in processes)):
+            break
+    if any((p.is_alive() for p in processes)):
+        print("Some processes timedout.")
+        for prc in processes:
+            prc.terminate()
+            prc.join()
     print("Games finished")
     
 
 if __name__ == "__main__":
     #n = 5
-    #play_games(1)
-    play_as_human(4)
+    play_games(1)
+    #play_as_human(4)
     
     
