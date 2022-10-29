243c96da61e7b2bd2bdd6188ffda1489a75cb70b
ilmari99 <i.vahteristo@gmail.com>
2022-10-29 12:54:18 +0300

    Added a RandomPlayer class, to establish a random baseline, as well as test the game engine

--- Moska/Player/RandomPlayer.py
+++ Moska/Player/RandomPlayer.py
@@ -1,4 +1,4 @@
-
+from __future__ import annotations
 import logging
 import random
 from Moska import utils
@@ -10,7 +10,7 @@ if TYPE_CHECKING:
 
 class RandomPlayer(AbstractPlayer):
     
-    def __init__(self, moskaGame: MoskaGame = None, pid: int = 0, name: str = "", delay=10 ** -6, requires_graphic: bool = False, debug: bool = False, log_level=logging.INFO, log_file=""):
+    def __init__(self, moskaGame : MoskaGame = None, pid: int = 0, name: str = "", delay=10 ** -6, requires_graphic: bool = False, debug: bool = False, log_level=logging.INFO, log_file=""):
         if not name:
             name = f"R-{pid}"
         super().__init__(moskaGame, pid, name, delay, requires_graphic, debug, log_level, log_file)
@@ -26,10 +26,10 @@ class RandomPlayer(AbstractPlayer):
         """
         poss_plays = {}
         for card in self.hand:
-            poss_plays[card] = self._map_to_list()
+            poss_plays[card] = self._map_to_list(card)
         out = {}
         for card, plays in poss_plays.items():
-            if random.random() > 0.3:
+            if plays and random.random() > 0.3:
                 play = random.choice(plays)
                 if play not in out.values():
                     out[card] = play
@@ -41,19 +41,19 @@ class RandomPlayer(AbstractPlayer):
         Returns:
             List[Card]: _description_
         """
-        return list(random.choice(self.hand))
+        return [random.choice(self.hand.cards)]
     
     def play_to_self(self) -> List[Card]:
-        playb_vals = self._playable_values_to_table()
-        if random.random() < 0.3:
-            return []
-        return list(random.choice(playb_vals))
+        playb_vals = self._playable_values_from_hand()
+        chand = self.hand.copy()
+        play_val = playb_vals.pop()
+        return chand.pop_cards(cond=lambda x : x.value == play_val,max_cards=random.randint(0,len(self.hand)))
     
     def play_to_target(self) -> List[Card]:
-        playb_vals = self._playable_values_to_table()
-        if random.random() < 0.3:
-            return []
-        return list(random.choice(playb_vals))
+        playb_vals = self._playable_values_from_hand()
+        chand = self.hand.copy()
+        play_val = playb_vals.pop()
+        return chand.pop_cards(cond=lambda x : x.value == play_val,max_cards=random.randint(0,self._fits_to_table()))
     
     def deck_lift_fall_method(self, deck_card: Card) -> Tuple[Card, Card]:
         return (deck_card, random.choice(self.moskaGame.cards_to_fall))
