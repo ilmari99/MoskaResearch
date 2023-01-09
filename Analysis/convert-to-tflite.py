b41c8a34782e0a7a3ccbba8a58bdcadb3a72c32a
ilmari99 <i.vahteristo@gmail.com>
2023-01-09 23:38:19 +0200

    Added shebang and cleared

--- Analysis/convert-to-tflite.py
+++ Analysis/convert-to-tflite.py
@@ -13,7 +13,7 @@ model = tf.keras.models.load_model(file_path)
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 
-with open("./ModelMB2/model.tflite", "wb") as f:
+with open(output_file, "wb") as f:
     f.write(tflite_model)
 
 
