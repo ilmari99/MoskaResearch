ee63a21fdffb0be2af7427c9b9c140b7bae2f788
ilmari99 <i.vahteristo@gmail.com>
2023-01-09 23:33:53 +0200

    Added prefetching of data, with tf.data.AUTOTUNE

--- Analysis/train_model.py
+++ Analysis/train_model.py
@@ -65,29 +65,42 @@ def get_nn_model(channels=None):
         )
     return model
 
-def get_loaded_model() -> tf.keras.models.Sequential:
-    model = tf.keras.models.load_model("./Model5-300/model.h5",compile=True)
+def get_loaded_model(path) -> tf.keras.models.Sequential:
+    model = tf.keras.models.load_model(path,compile=True)
     return model
 
+def get_transfer_model():
+    base_model = tf.keras.models.load_model("./Model5-300/model.h5",compile=True)
+    base_model.trainable = False
+    inputs = tf.keras.layers.Input(shape=(425,))
+    x = tf.keras.layers.Dense(425, activation="relu")(inputs)
+    output = base_model(x, training=False)
+    new_model = tf.keras.Model(inputs, output)
+    new_model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001, amsgrad=True),
+        metrics = ["accuracy"],
+        loss = tf.keras.losses.BinaryCrossentropy(from_logits=False,label_smoothing=0)
+    )
+    return new_model
+
 if __name__ == "__main__":
-    all_dataset = create_tf_dataset("./RandomLogs/Vectors/",add_channel=False)
-    model = get_optimal_model()
+    all_dataset = create_tf_dataset("./Data/MB1Logs168k/Logs/Vectors/",add_channel=False)
+    model = get_loaded_model("./Model6-39/model.h5")
     VALIDATION_LENGTH = 100000
     TEST_LENGTH = 100000
     BATCH_SIZE = 4096
     SHUFFLE_BUFFER_SIZE = 100000
-    print(all_dataset.element_spec)
     
     validation_ds = all_dataset.take(VALIDATION_LENGTH).batch(BATCH_SIZE)
     
     test_ds = all_dataset.skip(VALIDATION_LENGTH).take(TEST_LENGTH).batch(BATCH_SIZE)
     
-    train_ds = all_dataset.skip(VALIDATION_LENGTH+TEST_LENGTH).shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)#Add shuffle for NN
+    train_ds = all_dataset.skip(VALIDATION_LENGTH+TEST_LENGTH).shuffle(SHUFFLE_BUFFER_SIZE).prefetch(tf.data.AUTOTUNE).batch(BATCH_SIZE)#Add shuffle for NN
     
     early_stopping_cb = tf.keras.callbacks.EarlyStopping(min_delta=0, patience=5)
-    if os.path.exists("tensorboard-logs/"):
+    if os.path.exists("tensorboard-log/"):
         raise Exception("Tensorboard log directory already exists")
-    tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir="tensorboard-logs/")
+    tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir="tensorboard-log/")
     
     model.fit(x=train_ds, validation_data=validation_ds, epochs=200, callbacks=[early_stopping_cb, tensorboard_cb])
     
