e21f78ef822f0db412cdd76a01bfed0806581961
ilmari99 <i.vahteristo@gmail.com>
2023-01-09 23:33:19 +0200

    Added shebang and cleared

--- Analysis/convert-to-tflite.py
+++ Analysis/convert-to-tflite.py
@@ -1,10 +1,19 @@
+#!/usr/bin/env python3
+import os
 import tensorflow as tf
 
-model = tf.keras.models.load_model("/home/ilmari/python/moska/Model6-39/model.h5")
+file_path = "/home/ilmari/python/moska/ModelMB2/model.h5"
+output_file = None
 
+if output_file is None:
+    output_file = os.path.splitext(file_path)[0] + ".tflite"
+print("Converting '{}' to '{}'".format(file_path, output_file))
+
+model = tf.keras.models.load_model(file_path)
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
-with open("./Model6-39/model.tflite", "wb") as f:
+
+with open("./ModelMB2/model.tflite", "wb") as f:
     f.write(tflite_model)
 
 
