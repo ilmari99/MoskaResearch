9ff3c9e00a834c660389c32cdf66e55cc016f22b
ilmari99 <i.vahteristo@gmail.com>
2023-01-13 19:23:23 +0200

    None

--- Analysis/optimize.py
+++ Analysis/optimize.py
@@ -25,6 +25,8 @@ def opt_data():
     
     return train_ds, validation_ds, test_ds
 
+
+
 def get_model(train_ds, validation_ds, test_ds):
         #os.system("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/")
         #os.system("export XLA_FLAGS=--xla_gpu_cuda_data_dir=/usr/local/cuda-11/")
