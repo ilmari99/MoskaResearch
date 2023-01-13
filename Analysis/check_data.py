2a176e5ea4a7d677f7455ede5479f23cd803246a
ilmari99 <i.vahteristo@gmail.com>
2023-01-13 19:21:05 +0200

    Removed deprecated functions, designed for reading from a single file. Now check_data is te go-to function.

--- Analysis/check_data.py
+++ Analysis/check_data.py
@@ -5,7 +5,7 @@ import re
 import pandas as pd
 import tensorflow as tf
 
-def create_tf_dataset(paths, add_channel=False) -> tf.data.Dataset:
+def create_tf_dataset(paths, norm=False, add_channel=False) -> tf.data.Dataset:
     """ Create a tf dataset from a folder of files"""
     if not isinstance(paths, (list, tuple)):
         try:
@@ -20,38 +20,32 @@ def create_tf_dataset(paths, add_channel=False) -> tf.data.Dataset:
     print("Number of files: " + str(len(file_paths)))
     random.shuffle(file_paths)
     print("Shuffled files.")
-    print(file_paths[0:10])
     dataset = tf.data.TextLineDataset(file_paths)
     dataset = dataset.map(lambda x: tf.strings.split(x, sep=", "))
     dataset = dataset.map(lambda x: (tf.strings.to_number(x[:-1]), tf.strings.to_number(x[-1])))
+    if norm:
+        dataset = dataset.map(lambda x,y : (tf.divide(x,51),y))
     if add_channel:
         dataset = dataset.map(lambda x,y: (tf.expand_dims(x, axis=-1), tf.expand_dims(y, axis=-1)))
     return dataset
-    
-    
-
-def check_unique_vectors(path):
-    """ Check that there are no duplicate lines in files in path"""
-    with open(path,"r") as f:
-        lines = f.readlines()
-        uniq_lines = set(lines)
-        print(f"Number of unique lines: {len(uniq_lines)}")
-    print("Number of lines: " + str(len(lines)))
-    print("Percent unique data: " + str(len(uniq_lines)/len(lines)))
 
 
-def check_line_lengths_equal(path):
+
+def check_data(path, count_unique=False):
     line_length = -1
     out = True
     nlines = 0
     nlosses = 0
+    uniques = set()
     for file in os.listdir(path):
         with open(path+file,"r") as f:
             for line in f:
                 nlines += 1
+                if count_unique:
+                    uniques.add(line)
                 if line_length == -1:
                     line_length = line.count(",")
-                    print("Line length: " + str(line_length))
+                    print("Elements: " + str(line_length+1))
                 line = line.strip()
                 if line[-1] == "0":
                     nlosses += 1
@@ -61,22 +55,10 @@ def check_line_lengths_equal(path):
                     break
     if out:
         print(f"All {nlines} lines are the same length")
+    if count_unique:
+        print(f"Unique lines: {len(uniques)}, {(len(uniques)/nlines) * 100} %")
     print(f"Loss ratio: {nlosses/nlines}")
     return out
-
-def get_n_losses(path):
-    """ Get the distribution of winners/losses in the file"""
-    with open(path,"r") as f:
-        n_losses = 0
-        data_length = 0
-        for line in f:
-            data_length += 1
-            line = line.strip()
-            if line[-1] == "0":
-                n_losses += 1
-        print(f"{path} : {n_losses}")
-        print(f"Data length: {data_length}")
-        print(f"Loss ratio: {n_losses/data_length}")
 
 
 def combine_files(path,output="combined.csv"):
@@ -87,45 +69,7 @@ def combine_files(path,output="combined.csv"):
                 for line in f2:
                     f.write(line)
             f.write("\n")
-            
-def to_single_dataset(path,pickle_file="data.pkl"):
-    """ Convert a bunch of files to an hdf5 file.
-    Shuffle and balance the data and save it.
-    """
-    data = pd.read_csv(path)
-    winners = data[data.iloc[:,-1] == 1]
-    losers = data[data.iloc[:,-1] == 0]
-    winners = winners.sample(n=losers.shape[0])
-    print(f"Winners: {winners.shape}")
-    print(f"Losers: {losers.shape}")
-    data = pd.concat([winners, losers],axis=0,ignore_index=True)
-    data = data.sample(frac=1).reset_index(drop=True)
-    data.to_pickle(pickle_file)
-    print(f"Saved data to '{pickle_file}'")
-    print(data.head())
-    print(data.describe())
 
-def balance_data(path):
-    """ Balance the data by removing the extra 1s"""
-    ftype = path.split(".")[-1]
-    fname = path.split(".")[0]
-    if ftype == "csv":
-        data = pd.read_csv(path)
-        data.to_pickle(fname +".pkl")
-        print(f"Converted {path} to pkl")
-    elif ftype == "pkl":
-        data = pd.read_pickle(path)
-    winners = data[data.iloc[:,-1] == 1]
-    losers = data[data.iloc[:,-1] == 0]
-    winners = winners.sample(n=losers.shape[0])
-    print(f"Winners: {winners.shape}")
-    print(f"Losers: {losers.shape}")
-    data = pd.concat([winners, losers],axis=0)
-    data = data.sample(frac=1).reset_index(drop=True)
-    data.to_pickle(f"balanced-{fname}.pkl")
-    print(f"Saved data to 'balanced-{fname}.pkl'")
-    print(data.head())
-    print(data.describe())
     
 def find_duplicate_files(remove=False):
     folder_one = "./MB1Logs/Logs/Vectors/"
@@ -139,17 +83,17 @@ def find_duplicate_files(remove=False):
     count = 0
     for i,file in enumerate(duplicate_files):
         os.remove(folder_two + file)
-        if i % 100 == 0:
+        if i % 10 == 0:
             print(f"{i/len(duplicate_files)*100}% complete")
     return
 
 if __name__ == "__main__":
     CWD = os.getcwd()
-    PATH = "./Data/MB1Logs168k/Logs/Vectors/"
+    PATH = "./Logs/Vectors/"
     print("Current working directory: " + CWD)
     #find_duplicate_files()
     #balance_data()
-    check_line_lengths_equal(PATH)
+    check_data(PATH, count_unique=True)
     #combine_files(PATH,output="combined.csv")
     #get_n_losses("combined.csv")
     #check_unique_vectors("combined.csv")
