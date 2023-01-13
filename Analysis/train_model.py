2a75a3196e60032c3d88b33a5666d758418c693b
ilmari99 <i.vahteristo@gmail.com>
2023-01-13 19:26:03 +0200

    Experimentation. TODO: change input size to a CONSTANT for easy changing

--- Analysis/train_model.py
+++ Analysis/train_model.py
@@ -28,36 +28,22 @@ def get_optimal_model():
 
 
 def get_nn_model(channels=None):
-    #norm_layer = tf.keras.layers.Normalization(axis=2)
-    #norm_layer.adapt(x_train)
-    #print(norm_layer.adapt_mean, norm_layer.adapt_variance)
     model = tf.keras.models.Sequential([
     #norm_layer,
-        #norm_layer,
-        #tf.keras.layers.Dense(1024, activation="tanh",kernel_regularizer="l2"),
-        tf.keras.layers.BatchNormalization(axis=-1,input_shape=(425,)),
-        tf.keras.layers.Dense(425, activation="relu"),
-        #tf.keras.layers.Dense(425, activation="relu"),
-        #tf.keras.layers.Dense(425, activation="relu"),
-        #tf.keras.layers.Dense(425, activation="relu"),
-        tf.keras.layers.Dropout(0.4),
-        #tf.keras.layers.Dense(425, activation="relu"),
-        #tf.keras.layers.Dense(425, activation="relu"),
+        tf.keras.layers.Input(shape=(425,)),
+        tf.keras.layers.BatchNormalization(axis=-1,input_shape=(425,),),
         tf.keras.layers.Dense(425, activation="relu"),
         tf.keras.layers.Dropout(0.4),
-        #tf.keras.layers.Dense(425, activation="relu"),
-        #tf.keras.layers.Dense(425, activation="relu"),
+        tf.keras.layers.Dense(425, activation="relu"),
+        tf.keras.layers.Dropout(0.4),
         tf.keras.layers.Dense(425, activation="relu"),
         tf.keras.layers.Dropout(0.3),
-        #tf.keras.layers.Dense(425, activation="relu"),
-        #tf.keras.layers.Dense(425, activation="relu"),
-        #tf.keras.layers.Dense(425, activation="relu"),
         tf.keras.layers.Dense(425, activation="relu"),
         tf.keras.layers.Dense(1, activation="sigmoid")
     ])
 
     model.compile(
-        optimizer=tf.keras.optimizers.Adam(learning_rate=0.0007, amsgrad=True),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001, amsgrad=False),
         loss=tf.keras.losses.BinaryCrossentropy(from_logits=False,label_smoothing=0),
         #loss=tf.keras.losses.BinaryFocalCrossentropy(from_logits=False,gamma=2),
         #loss=tf.keras.losses.MeanSquaredError(),
@@ -68,7 +54,8 @@ def get_nn_model(channels=None):
 def get_loaded_model(path) -> tf.keras.models.Sequential:
     model = tf.keras.models.load_model(path,compile=True)
     return model
-
+    
+    
 def get_transfer_model():
     base_model = tf.keras.models.load_model("./Model5-300/model.h5",compile=True)
     base_model.trainable = False
@@ -84,12 +71,12 @@ def get_transfer_model():
     return new_model
 
 if __name__ == "__main__":
-    all_dataset = create_tf_dataset("./Data/MB1Logs168k/Logs/Vectors/",add_channel=False)
-    model = get_loaded_model("./Model6-39/model.h5")
+    all_dataset = create_tf_dataset(["./Data/MB1Logs168k/Logs/Vectors/", "Data/BigLogs/Logs/Vectors/"],add_channel=False)
+    model = get_loaded_model("ModelMB5/model.h5")#loaded_model("./Model6-39/model.h5")
     VALIDATION_LENGTH = 100000
     TEST_LENGTH = 100000
-    BATCH_SIZE = 4096
-    SHUFFLE_BUFFER_SIZE = 100000
+    BATCH_SIZE = 1024
+    SHUFFLE_BUFFER_SIZE = 10000
     
     validation_ds = all_dataset.take(VALIDATION_LENGTH).batch(BATCH_SIZE)
     
@@ -97,7 +84,7 @@ if __name__ == "__main__":
     
     train_ds = all_dataset.skip(VALIDATION_LENGTH+TEST_LENGTH).shuffle(SHUFFLE_BUFFER_SIZE).prefetch(tf.data.AUTOTUNE).batch(BATCH_SIZE)#Add shuffle for NN
     
-    early_stopping_cb = tf.keras.callbacks.EarlyStopping(min_delta=0, patience=5)
+    early_stopping_cb = tf.keras.callbacks.EarlyStopping(min_delta=0, patience=10, restore_best_weights=True, start_from_epoch=10)
     if os.path.exists("tensorboard-log/"):
         raise Exception("Tensorboard log directory already exists")
     tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir="tensorboard-log/")
