adbf340bceb4d3038043d9305179db0153961c5b
ilmari99 <i.vahteristo@gmail.com>
2023-01-20 22:35:48 +0200

    Removed unused imports

--- Moska/Player/NNEvaluatorBot.py
+++ Moska/Player/NNEvaluatorBot.py
@@ -1,11 +1,6 @@
 from __future__ import annotations
-from collections import Counter, namedtuple
-import itertools
+from collections import namedtuple
 import logging
-import random
-import threading
-import time
-import warnings
 import numpy as np
 from .AbstractEvaluatorBot import AbstractEvaluatorBot
 from typing import Dict, List,TYPE_CHECKING, Tuple
@@ -13,12 +8,6 @@ from ..Game.GameState import FullGameState, GameState
 if TYPE_CHECKING:
     from ..Game.Deck import Card
     from ..Game.Game import MoskaGame
-
-# Create a named tuple, to be able to compare assignments.
-# The assignments are the same, if the same cards are played to the same cards, regardless of order.
-# This can be checked by sorting the indices, and comparing the sorted lists.
-Assignment = namedtuple("Assignment", ["inds"])
-Assignment.__eq__ = lambda self, other : self.inds.copy().sort() == other.inds.copy().sort()
 
 class NNEvaluatorBot(AbstractEvaluatorBot):
     
@@ -32,17 +21,19 @@ class NNEvaluatorBot(AbstractEvaluatorBot):
                  max_num_states : int = 1000,
                  normalize: bool = True,
                  pred_format : str ="new",
+                 model_id : (str or int) = "all"
                  ):
         self.normalize = normalize
         self.pred_format = pred_format
         self.max_num_states = max_num_states
+        self.model_id = model_id
         if not name:
             name = "NNEV"
         super().__init__(moskaGame, name, delay, requires_graphic, log_level, log_file, max_num_states)
         
     def evaluate_states(self, states: List[FullGameState]) -> List[float]:
         state_vectors = [state.as_perspective_vector(self, norm=self.normalize,fmt=self.pred_format) for state in states]
-        preds = self.moskaGame.model_predict(np.array(state_vectors, dtype=np.float32))
+        preds = self.moskaGame.model_predict(np.array(state_vectors, dtype=np.float32), model_id=self.model_id)
         return preds
 
 
