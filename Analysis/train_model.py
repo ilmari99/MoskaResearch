b2dcc13d008f3d99a7408c17b672c9e80fc3f05e
ilmari99 <i.vahteristo@gmail.com>
2023-01-20 22:36:37 +0200

    Update

--- Analysis/train_model.py
+++ Analysis/train_model.py
@@ -8,8 +8,9 @@ import sys
 from check_data import create_tf_dataset
 
 def get_optimal_model():
+    global INPUT_SHAPE
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Input(shape=(425,)))
+    model.add(tf.keras.layers.Input(shape=INPUT_SHAPE))
     model.add(tf.keras.layers.BatchNormalization(axis=-1))
     model.add(tf.keras.layers.Dense(units=425, activation="relu",kernel_regularizer=None))
     model.add(tf.keras.layers.Dropout(rate=0.226))
@@ -28,25 +29,24 @@ def get_optimal_model():
 
 
 def get_nn_model(channels=None):
+    global INPUT_SHAPE
     model = tf.keras.models.Sequential([
     #norm_layer,
-        tf.keras.layers.Input(shape=(425,)),
-        tf.keras.layers.BatchNormalization(axis=-1,input_shape=(425,),),
-        tf.keras.layers.Dense(425, activation="relu"),
+        tf.keras.layers.Input(shape=INPUT_SHAPE),
+        #tf.keras.layers.BatchNormalization(axis=-1),
+        tf.keras.layers.Dense(600, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(600, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(500, activation="relu"),
         tf.keras.layers.Dropout(0.4),
-        tf.keras.layers.Dense(425, activation="relu"),
-        tf.keras.layers.Dropout(0.4),
-        tf.keras.layers.Dense(425, activation="relu"),
-        tf.keras.layers.Dropout(0.3),
-        tf.keras.layers.Dense(425, activation="relu"),
+        tf.keras.layers.Dense(400, activation="relu"),
         tf.keras.layers.Dense(1, activation="sigmoid")
     ])
 
     model.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001, amsgrad=False),
         loss=tf.keras.losses.BinaryCrossentropy(from_logits=False,label_smoothing=0),
-        #loss=tf.keras.losses.BinaryFocalCrossentropy(from_logits=False,gamma=2),
-        #loss=tf.keras.losses.MeanSquaredError(),
         metrics=['accuracy']
         )
     return model
@@ -56,11 +56,15 @@ def get_loaded_model(path) -> tf.keras.models.Sequential:
     return model
     
     
-def get_transfer_model():
-    base_model = tf.keras.models.load_model("./Model5-300/model.h5",compile=True)
+def get_transfer_model(base_model_path):
+    global INPUT_SHAPE
+    base_model = tf.keras.models.load_model(base_model_path,compile=True)
     base_model.trainable = False
-    inputs = tf.keras.layers.Input(shape=(425,))
-    x = tf.keras.layers.Dense(425, activation="relu")(inputs)
+    inputs = tf.keras.layers.Input(shape=INPUT_SHAPE)
+    x = tf.keras.layers.Flatten()(inputs)
+    x = tf.keras.layers.Dense(431, activation="linear")(inputs)
+    x = tf.keras.layers.Dropout(0.4)(x)
+    x = tf.keras.layers.Dense(425, activation="linear")(x)
     output = base_model(x, training=False)
     new_model = tf.keras.Model(inputs, output)
     new_model.compile(
@@ -70,27 +74,51 @@ def get_transfer_model():
     )
     return new_model
 
+
+def load_from_checkpoint(model : tf.keras.models.Sequential, checkpoint_path : str) -> tf.keras.models.Sequential:
+    model.load_weights(checkpoint_path)
+    return model
+
+INPUT_SHAPE = (430,)
 if __name__ == "__main__":
-    all_dataset = create_tf_dataset(["./Data/MB1Logs168k/Logs/Vectors/", "Data/BigLogs/Logs/Vectors/"],add_channel=False)
-    model = get_loaded_model("ModelMB5/model.h5")#loaded_model("./Model6-39/model.h5")
+    all_dataset = create_tf_dataset(["./3ModelsLog/Vectors/", "./1ModelsLog/Vectors/"],
+                                    add_channel=False,
+                                    norm=False,
+                                    )
+    print(all_dataset.take(1).as_numpy_iterator().next())
+    model = get_nn_model()
     VALIDATION_LENGTH = 100000
     TEST_LENGTH = 100000
-    BATCH_SIZE = 1024
-    SHUFFLE_BUFFER_SIZE = 10000
+    BATCH_SIZE = 4096
+    tensorboard_log = "tensorboard-log/"
+    checkpoint_filepath = './model-checkpoints/'
     
     validation_ds = all_dataset.take(VALIDATION_LENGTH).batch(BATCH_SIZE)
     
     test_ds = all_dataset.skip(VALIDATION_LENGTH).take(TEST_LENGTH).batch(BATCH_SIZE)
     
-    train_ds = all_dataset.skip(VALIDATION_LENGTH+TEST_LENGTH).shuffle(SHUFFLE_BUFFER_SIZE).prefetch(tf.data.AUTOTUNE).batch(BATCH_SIZE)#Add shuffle for NN
+    train_ds = all_dataset.skip(VALIDATION_LENGTH+TEST_LENGTH).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)#Add shuffle for NN
     
-    early_stopping_cb = tf.keras.callbacks.EarlyStopping(min_delta=0, patience=10, restore_best_weights=True, start_from_epoch=10)
     if os.path.exists("tensorboard-log/"):
         raise Exception("Tensorboard log directory already exists")
-    tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir="tensorboard-log/")
     
-    model.fit(x=train_ds, validation_data=validation_ds, epochs=200, callbacks=[early_stopping_cb, tensorboard_cb])
+    early_stopping_cb = tf.keras.callbacks.EarlyStopping(min_delta=0, patience=20, restore_best_weights=True, start_from_epoch=10)
+    tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir="tensorboard-log/",histogram_freq=1,profile_batch=(50,100),)
+    model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_filepath,
+        save_weights_only=True,
+        monitor='val_loss',
+        mode='min',
+        save_best_only=True)
     
-    model.evaluate(test_ds, verbose=1)
+    
+    
+    model.fit(x=train_ds, 
+              validation_data=validation_ds, 
+              epochs=160, 
+              callbacks=[early_stopping_cb, tensorboard_cb, model_checkpoint_callback],
+              )
+    
+    model.evaluate(test_ds, verbose=2)
     
     model.save("model.h5")
