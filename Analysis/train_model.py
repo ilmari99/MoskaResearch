91ac294c4776c4654e530387ae67172af2869b47
ilmari86 <i.vahteristo@gmail.com>
2023-02-03 15:19:15 +0200

    Possibly improved branched model

--- Analysis/train_model.py
+++ Analysis/train_model.py
@@ -135,8 +135,6 @@ def get_misc_model(compile_=True):
 def get_branched_model():
     misc_parts = list(range(0,5)) + list(range(161,171))
     card_parts = list((n for n in range(0,430) if n not in misc_parts))
-    print(card_parts)
-    print(misc_parts)
     print(f"In total {len(card_parts)} card parts and {len(misc_parts)} misc parts: {len(card_parts)+len(misc_parts)}")
     inputs = tf.keras.layers.Input(shape=(430,))
     card_data = tf.gather(inputs, tf.constant(card_parts, dtype=tf.int32), axis=1)
@@ -144,34 +142,40 @@ def get_branched_model():
     misc_data = tf.gather(inputs,tf.constant(misc_parts, dtype=tf.int32), axis=1)
     
     # Model for card input (415,1); its like an image
-    img = tf.keras.layers.Conv1D(128,3,activation="linear")(card_data)
+    img = tf.keras.layers.BatchNormalization(axis=1)(card_data)
+    img = tf.keras.layers.Conv1D(32,3,activation="linear")(img)
     img = tf.keras.layers.LeakyReLU(alpha=0.3)(img)
-    img = tf.keras.layers.Conv1D(32,12, activation="linear")(img)
+    img = tf.keras.layers.Conv1D(32,6, activation="linear")(img)
     img = tf.keras.layers.LeakyReLU(alpha=0.3)(img)
     img = tf.keras.layers.Flatten()(img)
-    img = tf.keras.layers.Dense(400,activation="relu")(img)
-    img = tf.keras.layers.Dropout(rate=0.4)(img)
+    img = tf.keras.layers.Dense(600,activation="relu")(img)
+    img = tf.keras.layers.Dropout(rate=0.3)(img)
     img = tf.keras.layers.Dense(300,activation="relu")(img)
-    img_out = tf.keras.layers.Dense(1,activation="sigmoid")(img)
+    img = tf.keras.layers.Dense(100,activation="relu")(img)
+    # Output a vector, hopefully describing what are the benefits and weaknesses of the known cards
+    img_out = tf.keras.layers.Dense(20,activation="linear")(img)
     
-    # Model for misc input (15,)
+    # Small model for misc input (15,)
     misc = tf.keras.layers.Dense(15,activation="relu")(misc_data)
-    misc = tf.keras.layers.Dropout(rate=0.3)(misc)
     misc = tf.keras.layers.Dense(12,activation="relu")(misc)
-    misc = tf.keras.layers.Dropout(rate=0.3)(misc)
-    misc = tf.keras.layers.Dense(12,activation="relu")(misc)
-    misc_out = tf.keras.layers.Dense(1,activation="sigmoid")(misc)
+    # The output of this, only really matters, when cards in deck is low
+    # This output describes generally, how the position is good/bad
+    # Ie. can the player kopl, are they the target, which players are ready
+    misc_out = tf.keras.layers.Dense(5,activation="linear")(misc)
     
-    final_out = tf.keras.layers.Concatenate(axis=-1)([img_out,misc_out])
+    # Combine information from misc and card data
+    combined = tf.keras.layers.Concatenate(axis=-1)([img_out,misc_out])
+    combined = tf.keras.layers.Dense(25,activation="relu")(combined)
+    combined = tf.keras.layers.Dense(25,activation="relu")(combined)
     #final_out = tf.keras.layers.Flatten()(final_out)
-    final_out = tf.keras.layers.Dense(2,activation="linear")(final_out)
-    final_out = tf.keras.layers.Dense(1,activation="sigmoid")(final_out)
+    combined = tf.keras.layers.Dense(10,activation="relu")(combined)
+    combined_out = tf.keras.layers.Dense(1,activation="sigmoid")(combined)
     
-    model = tf.keras.models.Model(inputs=inputs, outputs=final_out, name="branched_model")
+    model = tf.keras.models.Model(inputs=inputs, outputs=combined_out, name="branched_model")
     
     model.compile(
-        optimizer=tf.keras.optimizers.Adam(learning_rate=0.00015, amsgrad=False),
-        loss=[tf.keras.losses.BinaryCrossentropy(from_logits=False,label_smoothing=0)],
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.00015, amsgrad=True),
+        loss=[tf.keras.losses.BinaryFocalCrossentropy(gamma=2,from_logits=False,label_smoothing=0)],
         metrics=['accuracy'],
         )
     return model
@@ -208,7 +212,7 @@ def get_conv_model():
 INPUT_SHAPE = (430,)
 if __name__ == "__main__":
     
-    all_dataset = create_tf_dataset(["./Logs2/Vectors/"],
+    all_dataset = create_tf_dataset(["./LastLogs/Vectors/"],
                                     add_channel=False,
                                     get_part="full"
                                     )
@@ -216,9 +220,9 @@ if __name__ == "__main__":
     print(all_dataset.take(1).as_numpy_iterator().next()[0].shape)
     #model = load_from_checkpoint(get_nn_model(),'./model-checkpoints/')
     model = get_branched_model()
-    VALIDATION_LENGTH = 128
-    TEST_LENGTH = 128
-    BATCH_SIZE = 128
+    VALIDATION_LENGTH = 100000
+    TEST_LENGTH = 100000
+    BATCH_SIZE = 4096
     tensorboard_log = "tensorboard-log/"
     checkpoint_filepath = './model-checkpoints/'
     model_file = "model.h5"    
