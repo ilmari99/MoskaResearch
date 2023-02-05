9510f05be02f44622ddb7f8b22492eb5d6ecee97
ilmari99 <i.vahteristo@gmail.com>
2023-02-05 15:30:21 +0200

    Created a script for training models. The script creates a directory specified by the models name (ex. ModelF5) and puts all the files used to train the model there.

--- Analysis/train_model.py
+++ Analysis/train_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import random
+import warnings
 import tensorflow as tf
 import numpy as np
 import pandas as pd
@@ -141,15 +142,14 @@ def get_branched_model():
     card_data = tf.expand_dims(card_data, axis=-1)
     misc_data = tf.gather(inputs,tf.constant(misc_parts, dtype=tf.int32), axis=1)
     
-    # Model for card input (415,1); Convolutions can be used for this, since there are patterns that can be found
+    # Model for card input (415,1); Convolutions can be used for this, since there are patterns that can>
     img = tf.keras.layers.BatchNormalization(axis=1)(card_data)
     img = tf.keras.layers.Conv1D(128,3,activation="linear")(img)
-    img = tf.keras.layers.LeakyReLU(alpha=0.3)(img)
-    img = tf.keras.layers.Conv1D(64,5,activation="linear")(img)
     img = tf.keras.layers.LeakyReLU(alpha=0.3)(img)
     img = tf.keras.layers.Conv1D(16,3,activation="linear")(img)
     img = tf.keras.layers.LeakyReLU(alpha=0.3)(img)
     img = tf.keras.layers.Flatten()(img)
+    img = tf.keras.layers.Dropout(rate=0.15)(img)
     img = tf.keras.layers.Dense(600,activation="relu")(img)
     img = tf.keras.layers.Dropout(rate=0.5)(img)
     img = tf.keras.layers.Dense(400,activation="relu")(img)
@@ -157,7 +157,7 @@ def get_branched_model():
     img = tf.keras.layers.Dense(200,activation="relu")(img)
     img = tf.keras.layers.Dropout(rate=0.4)(img)
     img = tf.keras.layers.Dense(200,activation="relu")(img)
-    # Output a vector, hopefully describing what are the benefits and weaknesses of the known cards (table, others, self)
+    # Output a vector, hopefully describing what are the benefits and weaknesses of the known cards (tab>
     img_out = tf.keras.layers.Dense(50,activation="relu")(img)
     
     # Small model for misc input (15,)
@@ -172,9 +172,9 @@ def get_branched_model():
     # Combine information from misc and card data
     combined = tf.keras.layers.Concatenate(axis=-1)([img_out,misc_out])
     combined = tf.keras.layers.Dense(75,activation="relu")(combined)
-    combined = tf.keras.layers.Dropout(rate=0.4)(combined)
+    combined = tf.keras.layers.Dropout(rate=0.5)(combined)
     combined = tf.keras.layers.Dense(75,activation="relu")(combined)
-    combined = tf.keras.layers.Dropout(rate=0.4)(combined)
+    combined = tf.keras.layers.Dropout(rate=0.5)(combined)
     combined = tf.keras.layers.Dense(65,activation="relu")(combined)
     combined_out = tf.keras.layers.Dense(1,activation="sigmoid")(combined)
     
@@ -182,7 +182,7 @@ def get_branched_model():
     
     model.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001, amsgrad=True),
-        loss=[tf.keras.losses.BinaryFocalCrossentropy(gamma = 1.5, from_logits=False,label_smoothing=0)],
+        loss=[tf.keras.losses.BinaryCrossentropy(from_logits=False,label_smoothing=0)],
         metrics=['accuracy'],
         )
     return model
@@ -219,8 +219,7 @@ def get_conv_model():
 
 INPUT_SHAPE = (430,)
 if __name__ == "__main__":
-    
-    all_dataset = create_tf_dataset(["./Data/LastLogs1/Vectors/", "./Data/LastLogs2/Vectors/"],
+    all_dataset = create_tf_dataset(["./Data/LastLogs2/Vectors/"],
                                     add_channel=False,
                                     get_part="full"
                                     )
@@ -228,21 +227,28 @@ if __name__ == "__main__":
     #model = load_from_checkpoint(get_nn_model(),'./model-checkpoints/')
     model = get_branched_model()
     print(model.summary())
-    VALIDATION_LENGTH = 100000
-    TEST_LENGTH = 100000
+    VALIDATION_LENGTH = 500000
+    TEST_LENGTH = 500000
     BATCH_SIZE = 4096
     tensorboard_log = "tensorboard-log/"
     checkpoint_filepath = './model-checkpoints/'
-    model_file = "model.h5"    
-
+    model_file = "model.h5" 
+    if len(sys.argv) > 1:
+           to_dir = sys.argv[1]
+           tensorboard_log = os.path.join(to_dir,tensorboard_log)
+           checkpoint_filepath = os.path.join(to_dir,checkpoint_filepath)
+           model_file = os.path.join(to_dir,model_file)
+    print("Tensorboard log directory: ",tensorboard_log)
+    print("Checkpoint directory: ",checkpoint_filepath)
+    print("Model file: ",model_file)
+    
+    all_dataset = all_dataset.take(VALIDATION_LENGTH+TEST_LENGTH+100000)
     validation_ds = all_dataset.take(VALIDATION_LENGTH).batch(BATCH_SIZE)
-    
     test_ds = all_dataset.skip(VALIDATION_LENGTH).take(TEST_LENGTH).batch(BATCH_SIZE)
-    
-    train_ds = all_dataset.skip(VALIDATION_LENGTH+TEST_LENGTH).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)#Add shuffle for NN
+    train_ds = all_dataset.skip(VALIDATION_LENGTH+TEST_LENGTH).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
     
     if os.path.exists(tensorboard_log):
-        raise Exception("Tensorboard log directory already exists")
+        warnings.warn("Tensorboard log directory already exists!")
     
     early_stopping_cb = tf.keras.callbacks.EarlyStopping(min_delta=0, patience=8, restore_best_weights=True, start_from_epoch=1)
     tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir=tensorboard_log,histogram_freq=5)
@@ -255,7 +261,7 @@ if __name__ == "__main__":
     
     model.fit(x=train_ds, 
               validation_data=validation_ds, 
-              epochs=100, 
+              epochs=50, 
               callbacks=[early_stopping_cb, tensorboard_cb, model_checkpoint_callback],
               )
     
