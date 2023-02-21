5b4d6742f5a71248f2d084bb353d7772399b09de
ilmari99 <i.vahteristo@gmail.com>
2023-02-21 17:33:36 +0000

    Previously reusable players were (class,Callable) types, not they are in this class which cleans code a bit.

--- Play/PlayerWrapper.py
+++ Play/PlayerWrapper.py
@@ -6,6 +6,7 @@ import time
 import sys
 from Moska.Player.AbstractPlayer import AbstractPlayer
 from typing import Any, Callable, Dict, Iterable, List, Tuple
+from Utils import replace_setting_values
 """
 This file contains the PlayerWrapper class, which is used to wrap a player class and settings into a single object.
 This object is then used to create a player instance. It is done this way to ease multiprocessing, and create filenames based on the game number.
@@ -15,28 +16,33 @@ class PlayerWrapper:
     """ Wraps a player class and settings into a single object.
     Avoid ugly code, which uses a tuple of (player_class, settings : Callable) everywhere.
     """
-    def __init__(self, player_class: AbstractPlayer, settings: Dict[str, Any]):
+    def __init__(self, player_class: AbstractPlayer, settings: Dict[str, Any], infer_log_file = False):
         """ Settings should have '{x}' somewhere in it, which will be replaced by the game number.
         """
         if not issubclass(player_class, AbstractPlayer):
             raise TypeError(f"Player class must be a subclass of AbstractPlayer, but is {player_class}")
         if not isinstance(settings, dict):
             raise TypeError(f"Settings must be a dict, but is {settings}")
-        
+        if infer_log_file and not 'log_file' in settings:
+            name = settings.get("name", player_class.__name__)
+            settings["log_file"] = "Game_{x}-" + name + ".log"
         self.player_class = player_class
         self.settings = settings.copy()
         return
     
-    def _replace_game_id(self, game_id: int) -> None:
-        """ Replace the '{x}' in the settings with the game id.
+    def _get_instance_settings(self, game_id : int) -> None:
+        """ Create a new settings dict, with the game id replaced.
         """
-        for key in self.settings.keys():
-            if isinstance(self.settings[key], str):
-                self.settings[key] = self.settings[key].format(game_id)
-        return
+        # Create a new dict, so that the original settings are not changed.
+        instance_settings = replace_setting_values(self.settings, game_id)
+        return instance_settings
     
-    def __call__(self, game_id : int = 0) -> AbstractPlayer:
+    def __repr__(self) -> str:
+        return f"PlayerWrapper({self.player_class.__name__}, {self.settings})"
+    
+    def __call__(self, game_id : int = 0,instance_settings = None) -> AbstractPlayer:
         """ Create a player instance.
         """
-        self._replace_game_id(game_id)
-        return self.player_class(**self.settings)
+        if not instance_settings:
+            instance_settings = self._get_instance_settings(game_id)
+        return self.player_class(**instance_settings)
