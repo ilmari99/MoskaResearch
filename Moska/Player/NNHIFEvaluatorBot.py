a2ff3a64937aa587d336440e8a1115bd1734c9ff
ilmari99 <i.vahteristo@gmail.com>
2023-04-16 20:08:37 +0100

    Tried to make bots play together against a single player. For some reason it doesn't seem to work too well. No benchmarking done though

--- Moska/Player/NNHIFEvaluatorBot.py
+++ Moska/Player/NNHIFEvaluatorBot.py
@@ -5,6 +5,7 @@ import numpy as np
 from .AbstractHIFEvaluatorBot import AbstractHIFEvaluatorBot
 from typing import Dict, List,TYPE_CHECKING, Tuple
 from ..Game.GameState import FullGameState
+import math
 if TYPE_CHECKING:
     from ..Game.Deck import Card
     from ..Game.Game import MoskaGame
@@ -40,7 +41,10 @@ class NNHIFEvaluatorBot(AbstractHIFEvaluatorBot):
                 min_pl = self.moskaGame.get_players_condition(lambda pl: pl.name == self.min_player)
                 if not min_pl:
                     raise ValueError(f"Player {self.min_player} does not exist")
+                if min_pl[0] is self:
+                    raise NameError(f"The player to minimize cant be self.")
                 self.checked_min_pl_exists = True
+                self.plog.info(f"Minimizing player '{self.min_player}' with pid {min_pl[0].pid}.")
             return self.evaluate_states_min_player_hif(states)
         else:
             return self.evaluate_states_hif(states)
@@ -52,6 +56,7 @@ class NNHIFEvaluatorBot(AbstractHIFEvaluatorBot):
         return preds
     
     def evaluate_states_min_player_hif(self, states: List[FullGameState]) -> List[float]:
+        raise DeprecationWarning(f"Minimizing a certain player is deprecated for now!!!")
         state_collections = []
         # Get each players state as a perspective vector
         for state in states:
@@ -62,13 +67,33 @@ class NNHIFEvaluatorBot(AbstractHIFEvaluatorBot):
         # Group the predictions by state
         grouped_predictions = [predictions[i:i+len(self.moskaGame.players)] for i in range(0, len(predictions), len(self.moskaGame.players))]
         preds = []
-        min_player = self.moskaGame.get_players_condition(lambda pl: pl.name == self.min_player)[0].pid
+        min_player = self.moskaGame.get_players_condition(lambda pl: pl.name == self.min_player)[0]
         # Use the prediction of all players except the min player as the prediction for the state.
         # This way this player chooses the move, which minimizes every other players chance to lose (maximizes win), except for the min player
         # This basically means that this player plays to only make the min_player lose,
         # and has access to all other players information, except for the min player
+        curr_states = [FullGameState.from_game(self.moskaGame, copy=False).as_perspective_vector(pl,fmt=self.pred_format) for pl in self.moskaGame.players]
+        curr_evals = self.moskaGame.model_predict(np.array(curr_states,dtype=np.float32),model_id=self.model_id).flatten().tolist()
+        def filter_preds(preds):
+            for i,p in enumerate(preds):
+                if self.moskaGame.players[i].rank is not None:
+                    continue
+                yield p
+        rel_curr_evals = [ev / sum(filter_preds(curr_evals)) for ev in curr_evals]
         for pl_preds in grouped_predictions:
-            pred = sum(pl_preds) - pl_preds[min_player]
+            rel_preds = [ev / sum(filter_preds(pl_preds)) for ev in pl_preds]
+            # If MY current evaluation is sufficiently good, or very bad, then only focus on winning.
+            # Also, if the min_player's evaluation is already bad enough, then also focus on own game.
+            if min_player.rank is None and rel_curr_evals[min_player.pid] > 0.1 and len(self.moskaGame.get_players_condition(lambda x : x.rank is None)) > 3:
+                #pred = sum([pr for i, pr in enumerate(rel_preds) if i != min_player.pid])
+                pred = 1 - rel_preds[min_player.pid]
+                #coeffs = [calc_coeffs(pr, i) for i, pr in enumerate(pl_preds)]
+                #pred = sum([coeffs[i] * pl_preds[i] for i,_ in enumerate(pl_preds)])
+                #pred = pl_preds[self.pid] - 0.8*pl_preds[min_player.pid]
+                #pred = min([pl_pred for i,pl_pred in enumerate(pl_preds) if i != min_player.pid])
+                #pred = pl_preds[self.pid] / sum(pl_preds)
+            else:
+                pred = pl_preds[self.pid]
             preds.append(pred)
         return preds
 
