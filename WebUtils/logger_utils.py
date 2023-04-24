d280217130ad30e98044785ba155413d7275e469
ilmari99 <i.vahteristo@gmail.com>
2023-04-24 12:41:49 +0100

    Creation of loggers

--- WebUtils/logger_utils.py
+++ WebUtils/logger_utils.py
@@ -1,4 +1,10 @@
 import logging
+import random
+import datetime
+import os
+""" Create a logger to log different events by user.
+A logger is day specific. It will create a new file for each day.
+"""
 
 class SessionLogger(logging.LoggerAdapter):
     """ This logger will add the username to the log message.
@@ -13,30 +19,19 @@ class SessionLogger(logging.LoggerAdapter):
             username = self.session['username']
         return f"[{username}]: {msg}", kwargs
     
-class Logger:
-    """ Creates a SessionLogger, that is either created or returned if already created.
+def init_logger(session):
+    """ Create a logger. This logger will not log Flask logs. Thye will go to terminal instead. 
     """
-    def __init__(self, session):
-        self.session = session
-        self.init_logger(session)
-
-    def get_logger(self):
-        return self.logger
-    
-    def init_logger(self,session):
-        """ Create a logger. This logger will not log Flask logs. Thye will go to terminal instead. 
-        """
-        #logging.basicConfig(level=logging.INFO, filename="app.log", filemode="a", format="%(asctime)s-%(levelname)s-%(message)s")
-        base_logger = logging.getLogger("app_logs")
-        fh = logging.FileHandler("app.log", mode="a")
-        form = logging.Formatter("%(asctime)s-%(levelname)s-%(message)s")
-        fh.setFormatter(form)
-        base_logger.addHandler(fh)
-        base_logger.setLevel(logging.INFO)
-        self.logger = SessionLogger(session,base_logger, {})
-        return self.logger
-    
-    def __new__(cls, session):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(Logger, cls).__new__(cls)
-        return cls.instance.init_logger(session)
+    #logging.basicConfig(level=logging.INFO, filename="app.log", filemode="a", format="%(asctime)s-%(levelname)s-%(message)s")
+    base_logger = logging.getLogger("app_logs"+str(random.randint(0,1000000)))
+    if not os.path.exists("./Logs"):
+        os.mkdir("./Logs")
+    # Log file for every six hours
+    log_file = "./Logs/" + "app"+datetime.datetime.now().strftime("%Y-%m-%d")+"-"+str(datetime.datetime.now().hour//6)+".log"
+    fh = logging.FileHandler(log_file, mode="a")
+    form = logging.Formatter("%(asctime)s-%(levelname)s-%(message)s")
+    fh.setFormatter(form)
+    base_logger.addHandler(fh)
+    base_logger.setLevel(logging.INFO)
+    logger = SessionLogger(session,base_logger, {})
+    return logger
