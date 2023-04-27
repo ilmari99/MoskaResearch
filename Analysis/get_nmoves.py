6bf0ad09fcd5bf6f92410df614862277440138cb
ilmari99 <i.vahteristo@gmail.com>
2023-04-27 20:48:11 +0100

    Updated

--- Analysis/get_nmoves.py
+++ Analysis/get_nmoves.py
@@ -1,5 +1,7 @@
+import os
 import matplotlib.pyplot as plt
 import pandas as pd
+import math
 
 
 def read_data(filename):
@@ -7,6 +9,8 @@ def read_data(filename):
     nmoves_list = []
     with open(filename, 'r') as f:
         for line in f:
+            if "NMOVES" not in line:
+                continue
             line = line.split("NMOVES: ")[1].strip()
             data = line.split(" , ")
             deck = int(data[0])
@@ -34,20 +38,44 @@ if __name__ == "__main__":
     #
     #
     #
-    
-    filename = "nmoves.txt"
-    deck_list, nmoves_list = read_data(filename)
-    df = pd.DataFrame({'deck': deck_list, 'nmoves': nmoves_list})
-    print("Data:\n",df.describe())
-    df = df[ df["nmoves"]>1 ]
-    print("Data with nmoves=1 removed:\n",df.describe())
-    
-    plt.hist(df["nmoves"], bins=200)
-    plt.figure()
-    plt.scatter(df["deck"], df["nmoves"])
-    df = df.groupby('deck').mean()
-    print("Moves per deck:\n",df)
-    plt.figure()
-    plt.scatter(df.index, df["nmoves"])
+    #path = "./Benchmark3/"
+    complexities = []
+    players = range(2,9)
+    for nplayers in players:
+        path = "./Benchmark3-{}PL/".format(nplayers)
+        game_datas = []
+        # Loop through files
+        for file in os.listdir(path):
+            if not os.path.isfile(path + file):
+                print("Not a file:",file)
+                continue
+            deck_list, nmoves_list = read_data(path + file)
+            if deck_list and nmoves_list:
+                game_datas.append(pd.DataFrame({'deck': deck_list, 'nmoves': nmoves_list}))
+        # remove all rows with just 1 move
+        game_datas = [game_data[game_data.nmoves > 1] for game_data in game_datas]
+
+        # Calculate average number made moves per game
+        n_made_moves = [len(game_data) for game_data in game_datas]
+        avg_n_made_moves = (sum(n_made_moves)*nplayers)/len(n_made_moves)
+        print("Average number of moves per game:",avg_n_made_moves)
+
+        # Average number of possible moves per move
+        n_possible_moves = [game_data.nmoves.mean() for game_data in game_datas]
+        avg_nmoves = sum(n_possible_moves)/len(n_possible_moves)
+        print("Average number of possible moves per move:",sum(n_possible_moves)/len(n_possible_moves))
+
+        # Game tree complexity
+        gtc = avg_nmoves**avg_n_made_moves
+        print(f"Average game tree complexity of {nplayers} player games:",gtc)
+        # in log10
+        complexities.append(math.log10(gtc))
+    fig, ax = plt.subplots()
+    ax.plot(players, complexities)
+    ax.set_xlabel(f"Number of players", fontsize=14)
+    ax.set_ylabel(f"Game tree complexity (log10)", fontsize=14)
+    ax.set_title(f"Game tree complexity calculated from 500 games", fontsize=14)
     plt.show()
+
+    
             
