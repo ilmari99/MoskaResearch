e0138a4da7ea23c256790fc92cf3e38332483d78
ilmari99 <i.vahteristo@gmail.com>
2023-05-12 21:08:28 +0100

    Now doesn't use unicode characters, when playing with 'in_web = True' argument

--- Moska/Game/Game.py
+++ Moska/Game/Game.py
@@ -427,7 +427,9 @@ class MoskaGame:
                         return x.name
                     elif isinstance(x,Callable):
                         return ""
-                    if self.in_web and isinstance(x,(list,tuple)) and len(x)>0 and isinstance(x[0],Card):
+                    if self.in_web and x and (isinstance(x,(list,tuple)) and isinstance(x[0],Card)) or (isinstance(x,dict) and isinstance(list(x.keys())[0], Card)):
+                        if isinstance(x,dict):
+                            return {k.as_str(symbol=False):v.as_str(symbol=False) for k,v in x.items()}
                         return [c.as_str(symbol=False) for c in x]
                     else:
                         return x
@@ -759,8 +761,11 @@ class MoskaGame:
             p_with_2.hand.add([trump_card])
             trump_card = p_with_2.hand.pop_cards(cond = lambda x : x.suit == self.trump and x.value == 2)[0]
             self.glog.info(f"Removed {trump_card} from {p_with_2.name}")
-            if self.has_graphics:
+            if self.in_console:
                 print(f"Player {p_with_2.name} had trump 2 in hand. Swapping {self._orig_trump_card} with {trump_card}")
+            if self.in_web:
+                # Write without unicode
+                print(f"Player {p_with_2.name} had trump 2 in hand. Swapping {self._orig_trump_card.as_str(symbol=False)} with {trump_card.as_str(symbol=False)}")
         self.trump_card = trump_card
         self.deck.place_to_bottom(self.trump_card)
         self.glog.info(f"Placed {self.trump_card} to bottom of deck.")
