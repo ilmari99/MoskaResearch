8553a29e8a0a74fe6ca5dbe5ff576193ee357e72
ilmari99 <i.vahteristo@gmail.com>
2023-09-10 16:57:19 +0300

    Benchmark on three Bot3s

--- ModelTraining/benchmark.py
+++ ModelTraining/benchmark.py
@@ -3,13 +3,13 @@ from MoskaEngine.Play.benchmark import BENCH3, clean_up
 from MoskaEngine.Play.PlayerWrapper import PlayerWrapper
 from MoskaEngine.Player.NNEvaluatorBot import NNEvaluatorBot
 
-good_pl_args = {"model_id":os.path.abspath(f"model_{100}.tflite"),
+good_pl_args = {"model_id":os.path.abspath(f"model_convV3.tflite"),
                 "max_num_states" : 1000,
                 "pred_format" : "bitmap",
                 }
 
 game_kwargs = {
-        "model_paths" : [os.path.abspath(f"model_{100}.tflite")],
+        "model_paths" : [os.path.abspath(f"model_convV3.tflite")],
 }
 
 print(good_pl_args)
@@ -17,4 +17,4 @@ print(game_kwargs)
 test_player = PlayerWrapper(NNEvaluatorBot, good_pl_args, infer_log_file=True, number=1)
 
 clean_up()
-BENCH3.run(test_player, cpus=10, chunksize=1, ngames=100, custom_game_kwargs=game_kwargs)
+BENCH3.run(test_player, cpus=15, chunksize=1, ngames=100, custom_game_kwargs=game_kwargs)
