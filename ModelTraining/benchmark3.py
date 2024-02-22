6753df84d1d47d62b07594134562d5b4af2015e0
ilmari99 <i.vahteristo@gmail.com>
2024-02-22 19:17:32 +0200

    Updated script

--- ModelTraining/benchmark3.py
+++ ModelTraining/benchmark3.py
@@ -2,21 +2,23 @@ import os
 from MoskaEngine.Play.benchmark import BENCH3, clean_up
 from MoskaEngine.Play.PlayerWrapper import PlayerWrapper
 from MoskaEngine.Player.NNEvaluatorBot import NNEvaluatorBot
+from MoskaEngine.Player.NNHIFEvaluatorBot import NNHIFEvaluatorBot
 
-good_pl_args = {"model_id":os.path.abspath(f"./Models/model-basic-from-1400k-nrnd-games.tflite"),
+good_pl_args = {"model_id":os.path.abspath(f"/home/ilmari/python/MoskaResearch/ModelTfliteFiles/2202_basic_top_V13_5256.tflite"),
                 "max_num_states" : 1000,
                 "pred_format" : "bitmap",
+                "name" : "test_player",
                 }
 
 game_kwargs = {
-        "model_paths" : [os.path.abspath(f"./Models/model-basic-from-1400k-nrnd-games.tflite")],
+        "model_paths" : [os.path.abspath(f"/home/ilmari/python/MoskaResearch/ModelTfliteFiles/2202_basic_top_V13_5256.tflite")],
 }
 
 print(good_pl_args)
 print(game_kwargs)
-test_player = PlayerWrapper(NNEvaluatorBot, good_pl_args, infer_log_file=True, number=1)
+test_player = PlayerWrapper(NNHIFEvaluatorBot, good_pl_args, infer_log_file=True, number=1)
 
 
 
 clean_up()
-BENCH3.run(test_player, cpus=15, chunksize=1, ngames=200, custom_game_kwargs=game_kwargs)
+BENCH3.run(test_player, cpus=15, chunksize=1, ngames=1000, custom_game_kwargs=game_kwargs)
